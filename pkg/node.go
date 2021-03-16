5cf4529cb360263bdac4c588756fcb18fd7afeea
Mikhail Borisov <mikhail.borisov@emc.com>
2021-03-16 18:02:43 +0000

    node daemonset
    Signed-off-by: Mikhail Borisov <mikhail.borisov@emc.com>

--- pkg/node.go
+++ pkg/node.go
@@ -1,10 +1,13 @@
 package pkg
 
 import (
+	"strconv"
+
 	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/utils/pointer"
 
@@ -12,16 +15,30 @@ import (
 )
 
 const (
-	csiName = "csi-baremetal"
-	componentName = "csi-baremetal-node"
-	prometheusPort = "8787"
-	serviceAccountName = "csi-node-sa"
+	csiName                       = "csi-baremetal"
+	componentName                 = "csi-baremetal-node"
+	prometheusPort                = 8787
+	serviceAccountName            = "csi-node-sa"
 	terminationGracePeriodSeconds = 10
-	loopbackManagerConfigName = "loopback-config"
-	
+	loopbackManagerConfigName     = "loopback-config"
+
 	// volumes
-	csiSocketDirVolume = "csi-socket-dir"
+	csiSocketDirVolume    = "csi-socket-dir"
 	registrationDirVolume = "registration-dir"
+	logsVolume            = "logs"
+	hostDevVolume         = "host-dev"
+	hostHomeVolume        = "host-home"
+	hostSysVolume         = "host-sys"
+	hostRootVolume        = "host-root"
+	hostRunUdevVolume     = "host-run-udev"
+	hostRunLVMVolume      = "host-run-lvm"
+	hostRunLock           = "host-run-lock"
+	mountPointDirVolume   = "mountpoint-dir"
+	csiPathVolume         = "csi-path"
+	driveConfigVolume     = "drive-config"
+
+	// ports
+	livenessPort = "liveness-port"
 )
 
 type Node struct {
@@ -47,7 +64,7 @@ func createNodeDaemonSet(namespace string) *v1.DaemonSet {
 	// todo split this definition
 	return &v1.DaemonSet{
 		ObjectMeta: metav1.ObjectMeta{Name: componentName, Namespace: namespace},
-		Spec:       appsv1.DaemonSetSpec{
+		Spec: appsv1.DaemonSetSpec{
 			// selector
 			Selector: &metav1.LabelSelector{
 				MatchLabels: map[string]string{"app": componentName},
@@ -58,19 +75,19 @@ func createNodeDaemonSet(namespace string) *v1.DaemonSet {
 				ObjectMeta: metav1.ObjectMeta{
 					// labels
 					Labels: map[string]string{
-						"app": componentName,
+						"app":                    componentName,
 						"app.kubernetes.io/name": csiName,
 					},
 					// integration with monitoring
 					Annotations: map[string]string{
 						"prometheus.io/scrape": "true",
-						"prometheus.io/port": prometheusPort,
-						"prometheus.io/path": "/metrics",
+						"prometheus.io/port":   strconv.Itoa(prometheusPort),
+						"prometheus.io/path":   "/metrics",
 					},
 				},
 				Spec: corev1.PodSpec{
-					Volumes:                       createNodeVolumes(),
-					Containers:                    createNodeContainers(),
+					Volumes:    createNodeVolumes(),
+					Containers: createNodeContainers(),
 					// todo what is the hack?
 					TerminationGracePeriodSeconds: pointer.Int64Ptr(terminationGracePeriodSeconds),
 					NodeSelector:                  map[string]string{},
@@ -86,31 +103,31 @@ func createNodeVolumes() []corev1.Volume {
 	// todo how to avoid this?
 	directory := corev1.HostPathDirectory
 	directoryOrCreate := corev1.HostPathDirectoryOrCreate
-	
+
 	return []corev1.Volume{
-		{Name: "logs", VolumeSource: corev1.VolumeSource{
+		{Name: logsVolume, VolumeSource: corev1.VolumeSource{
 			EmptyDir: &corev1.EmptyDirVolumeSource{},
 		}},
-		{Name: "host-dev", VolumeSource: corev1.VolumeSource{
+		{Name: hostDevVolume, VolumeSource: corev1.VolumeSource{
 			HostPath: &corev1.HostPathVolumeSource{Path: "/dev", Type: &directory},
 		}},
 		// todo this if for loopback manager only
-		{Name: "host-home", VolumeSource: corev1.VolumeSource{
+		{Name: hostHomeVolume, VolumeSource: corev1.VolumeSource{
 			HostPath: &corev1.HostPathVolumeSource{Path: "/home", Type: &directory},
 		}},
-		{Name: "host-sys", VolumeSource: corev1.VolumeSource{
+		{Name: hostSysVolume, VolumeSource: corev1.VolumeSource{
 			HostPath: &corev1.HostPathVolumeSource{Path: "/sys", Type: &directory},
 		}},
-		{Name: "host-root", VolumeSource: corev1.VolumeSource{
+		{Name: hostRootVolume, VolumeSource: corev1.VolumeSource{
 			HostPath: &corev1.HostPathVolumeSource{Path: "/", Type: &directory},
 		}},
-		{Name: "host-run-udev", VolumeSource: corev1.VolumeSource{
+		{Name: hostRunUdevVolume, VolumeSource: corev1.VolumeSource{
 			HostPath: &corev1.HostPathVolumeSource{Path: "/run/udev", Type: &directory},
 		}},
-		{Name: "host-run-lvm", VolumeSource: corev1.VolumeSource{
+		{Name: hostRunLVMVolume, VolumeSource: corev1.VolumeSource{
 			HostPath: &corev1.HostPathVolumeSource{Path: "/run/lvm", Type: &directory},
 		}},
-		{Name: "host-run-lock", VolumeSource: corev1.VolumeSource{
+		{Name: hostRunLock, VolumeSource: corev1.VolumeSource{
 			HostPath: &corev1.HostPathVolumeSource{Path: "/run/lock", Type: &directory},
 		}},
 		{Name: csiSocketDirVolume, VolumeSource: corev1.VolumeSource{
@@ -119,14 +136,14 @@ func createNodeVolumes() []corev1.Volume {
 		{Name: registrationDirVolume, VolumeSource: corev1.VolumeSource{
 			HostPath: &corev1.HostPathVolumeSource{Path: "/var/lib/kubelet/plugins_registry/", Type: &directoryOrCreate},
 		}},
-		{Name: "mountpoint-dir", VolumeSource: corev1.VolumeSource{
+		{Name: mountPointDirVolume, VolumeSource: corev1.VolumeSource{
 			HostPath: &corev1.HostPathVolumeSource{Path: "/var/lib/kubelet/pods", Type: &directory},
 		}},
-		{Name: "csi-path", VolumeSource: corev1.VolumeSource{
+		{Name: csiPathVolume, VolumeSource: corev1.VolumeSource{
 			HostPath: &corev1.HostPathVolumeSource{Path: "/var/lib/kubelet/plugins/kubernetes.io/csi"},
 		}},
 		// todo optional
-		{Name: "drive-config", VolumeSource: corev1.VolumeSource{
+		{Name: driveConfigVolume, VolumeSource: corev1.VolumeSource{
 			ConfigMap: &corev1.ConfigMapVolumeSource{LocalObjectReference: corev1.LocalObjectReference{
 				Name: loopbackManagerConfigName}},
 		}},
@@ -134,21 +151,24 @@ func createNodeVolumes() []corev1.Volume {
 }
 
 func createNodeContainers() []corev1.Container {
+	// todo get rid of this hack
+	bidirectional := corev1.MountPropagationBidirectional
 	return []corev1.Container{
 		{
-			Name: "liveness-probe",
-		 	Image: "livenessprobe:v2.1.0",
-		 	ImagePullPolicy: corev1.PullIfNotPresent,
-		 	Args: []string{"--csi-address=/csi/csi.sock"},
-		 	VolumeMounts: []corev1.VolumeMount{
+			Name:            "liveness-probe",
+			Image:           "livenessprobe:v2.1.0",
+			ImagePullPolicy: corev1.PullIfNotPresent,
+			Args:            []string{"--csi-address=/csi/csi.sock"},
+			VolumeMounts: []corev1.VolumeMount{
 				{Name: csiSocketDirVolume, MountPath: "/csi"},
 			},
 		},
 		{
-			Name: "csi-node-driver-registrar",
-			Image: "csi-node-driver-registrar:v1.0.1-gke.0",
+			Name:            "csi-node-driver-registrar",
+			Image:           "csi-node-driver-registrar:v1.0.1-gke.0",
 			ImagePullPolicy: corev1.PullIfNotPresent,
-			Args: []string{"--v=5", "--csi-address=$(ADDRESS)", "--kubelet-registration-path=$(DRIVER_REG_SOCK_PATH)"},
+			Args:            []string{"--v=5", "--csi-address=$(ADDRESS)",
+				"--kubelet-registration-path=$(DRIVER_REG_SOCK_PATH)"},
 			Lifecycle: &corev1.Lifecycle{PreStop: &corev1.Handler{Exec: &corev1.ExecAction{Command: []string{
 				"/bin/sh", "-c", "rm -rf /registration/csi-baremetal /registration/csi-baremetal-reg.sock"}}}},
 			Env: []corev1.EnvVar{
@@ -161,6 +181,70 @@ func createNodeContainers() []corev1.Container {
 			VolumeMounts: []corev1.VolumeMount{
 				{Name: csiSocketDirVolume, MountPath: "/csi"},
 				{Name: registrationDirVolume, MountPath: "/registration"},
+			},
+		},
+		{
+			Name:            "node",
+			Image:           "csi-baremetal-node:green",
+			ImagePullPolicy: corev1.PullIfNotPresent,
+			Args: []string{
+				"--csiendpoint=$(CSI_ENDPOINT)",
+				"--nodename=$(KUBE_NODE_NAME)",
+				"--namespace=$(NAMESPACE)",
+				"--extender=true",
+				"--usenodeannotation=true",
+				"--loglevel=info",
+				"--metrics-address=:%d" + strconv.Itoa(prometheusPort),
+				"--metrics-path=/metrics",
+				"--drivemgrendpoint=tcp://localhost:8888",
+			},
+			Ports: []corev1.ContainerPort{
+				{Name: livenessPort, ContainerPort: 9808, Protocol: corev1.ProtocolTCP},
+				{Name: "metrics", ContainerPort: prometheusPort, Protocol: corev1.ProtocolTCP},
+			},
+			LivenessProbe: &corev1.Probe{
+				Handler: corev1.Handler{HTTPGet: &corev1.HTTPGetAction{
+					Path: "/healthz",
+					Port: intstr.FromString(livenessPort)}},
+				InitialDelaySeconds: 300,
+				TimeoutSeconds:      3,
+				PeriodSeconds:       10,
+				FailureThreshold:    5,
+			},
+			ReadinessProbe: &corev1.Probe{
+				Handler: corev1.Handler{Exec: &corev1.ExecAction{Command: []string{
+					"/health_probe",
+					"-addr=:9999"}}},
+				InitialDelaySeconds: 3,
+				PeriodSeconds:       3,
+				SuccessThreshold:    3,
+				FailureThreshold:    100,
+			},
+			Env: []corev1.EnvVar{
+				{Name: "CSI_ENDPOINT", Value: "unix:///csi/csi.sock"},
+				{Name: "LOG_FORMAT", Value: "text"},
+				{Name: "KUBE_NODE_NAME", ValueFrom: &corev1.EnvVarSource{
+					FieldRef: &corev1.ObjectFieldSelector{APIVersion: "v1", FieldPath: "spec.nodeName"},
+				}},
+				{Name: "MY_POD_IP", ValueFrom: &corev1.EnvVarSource{
+					FieldRef: &corev1.ObjectFieldSelector{FieldPath: "status.podIP"},
+				}},
+				{Name: "NAMESPACE", ValueFrom: &corev1.EnvVarSource{
+					FieldRef: &corev1.ObjectFieldSelector{APIVersion: "v1", FieldPath: "metadata.namespace"},
+				}},
+			},
+			SecurityContext: &corev1.SecurityContext{Privileged: pointer.BoolPtr(true)},
+			VolumeMounts: []corev1.VolumeMount{
+				{Name: logsVolume, MountPath: "/var/log"},
+				{Name: hostDevVolume, MountPath: "/dev"},
+				{Name: hostSysVolume, MountPath: "/sys"},
+				{Name: hostRunUdevVolume, MountPath: "/run/udev"},
+				{Name: hostRunLVMVolume, MountPath: "/run/lvm"},
+				{Name: hostRunLock, MountPath: "/run/lock"},
+				{Name: csiSocketDirVolume, MountPath: "/csi"},
+				{Name: mountPointDirVolume, MountPath: "/var/lib/kubelet/pods", MountPropagation: &bidirectional},
+				{Name: csiPathVolume, MountPath: "/var/lib/kubelet/plugins/kubernetes.io/csi", MountPropagation: &bidirectional},
+				{Name: hostRootVolume, MountPath: "/hostroot", MountPropagation: &bidirectional},
 			},
 		},
 	}
