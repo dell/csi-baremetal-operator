8260c4749ea11b2388ae63f264df89a8f346e8a2
safronovD <danil_safronov@dell.com>
2021-03-31 17:29:19 +0300

    add finalizer

--- controllers/deployment_controller.go
+++ controllers/deployment_controller.go
@@ -18,12 +18,14 @@ package controllers
 
 import (
 	"context"
+	"strings"
 
 	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 
@@ -36,11 +38,15 @@ import (
 
 // DeploymentReconciler reconciles a Deployment object
 type DeploymentReconciler struct {
-	client.Client
+	Client client.Client
 	Log    logr.Logger
 	Scheme *runtime.Scheme
 	pkg.CSIDeployment
 }
+
+const (
+	csiFinalizer = "dell.emc.csi/csi-deployment-cleanup"
+)
 
 // +kubebuilder:rbac:groups=csi-baremetal.dell.com,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=csi-baremetal.dell.com,resources=deployments/status,verbs=get;update;patch
@@ -50,7 +56,7 @@ func (r *DeploymentReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) 
 	log := r.Log.WithValues("deployment", req.NamespacedName)
 
 	deployment := new(csibaremetalv1.Deployment)
-	err := r.Get(ctx, client.ObjectKey{Name: req.Name, Namespace: req.Namespace}, deployment)
+	err := r.Client.Get(ctx, client.ObjectKey{Name: req.Name, Namespace: req.Namespace}, deployment)
 	if err != nil {
 		if errors.IsNotFound(err) {
 			// TODO set logLevel as Warn after changing log library
@@ -64,6 +70,30 @@ func (r *DeploymentReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) 
 	}
 	log.Info("Custom resource obtained")
 
+	if deployment.ObjectMeta.DeletionTimestamp.IsZero() {
+		// Instance is not being deleted, add the finalizer if not present
+		if !containsFinalizer(deployment) {
+			deployment.ObjectMeta.Finalizers = append(deployment.ObjectMeta.Finalizers, csiFinalizer)
+			if err = r.Client.Update(ctx, deployment); err != nil {
+				log.Error(err, "Error adding finalizer")
+				return ctrl.Result{Requeue: true}, err
+			}
+
+			log.Info("Successfully add finalizer to CR")
+		}
+	} else {
+		if containsFinalizer(deployment) {
+			deployment.ObjectMeta.Finalizers = deleteFinalizer(deployment)
+			if err = r.Client.Update(ctx, deployment); err != nil {
+				log.Error(err, "Error removing finalizer")
+				return ctrl.Result{Requeue: true}, err
+			}
+
+			log.Info("Successfully remove finalizer")
+			return ctrl.Result{}, nil
+		}
+	}
+
 	if err = r.CSIDeployment.Update(deployment, r.Scheme); err != nil {
 		log.Error(err, "Unable to update deployment")
 		return ctrl.Result{Requeue: true}, err
@@ -74,6 +104,11 @@ func (r *DeploymentReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) 
 
 func (r *DeploymentReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
+		WithOptions(controller.Options{
+			// only one instance of CSIDeployment is allowed to be installed
+			// concurrent reconciliation isn't supported
+			MaxConcurrentReconciles: 1,
+		}).
 		Watches(&source.Kind{Type: &csibaremetalv1.Deployment{}}, &handler.EnqueueRequestForObject{}).
 		Watches(&source.Kind{Type: &appsv1.Deployment{}}, &handler.EnqueueRequestForOwner{
 			IsController: true,
@@ -85,4 +120,24 @@ func (r *DeploymentReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		}).
 		For(&csibaremetalv1.Deployment{}).
 		Complete(r)
+}
+
+func containsFinalizer(csiDep *csibaremetalv1.Deployment) bool {
+	for _, finalizer := range csiDep.ObjectMeta.Finalizers {
+		if strings.Contains(finalizer, csiFinalizer) {
+			return true
+		}
+	}
+	return false
+}
+
+func deleteFinalizer(csiDep *csibaremetalv1.Deployment) []string {
+	var result []string
+	for _, finalizer := range csiDep.ObjectMeta.Finalizers {
+		if finalizer == csiFinalizer {
+			continue
+		}
+		result = append(result, finalizer)
+	}
+	return result
 }
