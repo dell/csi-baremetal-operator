f5cf7f44db72de1f8cd352dcf1e5661f4450225d
yurkov-anton <36742876+yurkov-anton@users.noreply.github.com>
2021-04-27 03:56:56 +0300

    Update variables.mk

--- variables.mk
+++ variables.mk
@@ -3,8 +3,8 @@ PROJECT          := csi-baremetal-operator
 
 ### version
 MAJOR            := 0
-MINOR            := 0
-PATCH            := 13
+MINOR            := 2
+PATCH            := 0
 PRODUCT_VERSION  ?= ${MAJOR}.${MINOR}.${PATCH}
 BUILD_REL_A      := $(shell git rev-list HEAD |wc -l)
 BUILD_REL_B      := $(shell git rev-parse --short HEAD)
