e0e6381ee26ff099079878952734fc47219f8d4d
yurkov-anton <36742876+yurkov-anton@users.noreply.github.com>
2021-04-28 11:04:05 +0300

    [Issue-372] add versions variable to makefile
    Signed-off-by: Yurkov Anton <anton.yurkov@dell.com>

---
+++ variables.mk
@@ -0,0 +1,26 @@
+# project name
+PROJECT          := csi-baremetal-operator
+
+### version
+MAJOR            := 0
+MINOR            := 2
+PATCH            := 0
+PRODUCT_VERSION  ?= ${MAJOR}.${MINOR}.${PATCH}
+BUILD_REL_A      := $(shell git rev-list HEAD |wc -l)
+BUILD_REL_B      := $(shell git rev-parse --short HEAD)
+BLD_CNT          := $(shell echo ${BUILD_REL_A})
+BLD_SHA          := $(shell echo ${BUILD_REL_B})
+RELEASE_STR      := ${BLD_CNT}.${BLD_SHA}
+FULL_VERSION     := ${PRODUCT_VERSION}-${RELEASE_STR}
+TAG              := ${FULL_VERSION}
+BRANCH           := $(shell git rev-parse --abbrev-ref HEAD)
+
+### go env vars
+GO_ENV_VARS     := GO111MODULE=on ${GOPRIVATE_PART} ${GOPROXY_PART}
+
+### custom variables that could be ommited
+GOPRIVATE_PART  :=
+GOPROXY_PART    := GOPROXY=https://proxy.golang.org,direct
+
+# override some of variables, optional file
+-include variables.override.mk
