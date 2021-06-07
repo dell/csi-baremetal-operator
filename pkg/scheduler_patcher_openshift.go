3e4b4c0fb2f5dc504966d03da12bb770ac3c9ac6
safronovD <danil_safronov@dell.com>
2021-06-07 17:46:57 +0300

    fixes
    Signed-off-by: safronovD <danil_safronov@dell.com>

--- pkg/scheduler_patcher_openshift.go
+++ pkg/scheduler_patcher_openshift.go
@@ -21,7 +21,7 @@ const (
    "apiVersion" : "v1",
    "extenders": [
         {
-            "urlPrefix": "http://127.0.0.1:$PORT",
+            "urlPrefix": "http://127.0.0.1:8889",
             "filterVerb": "filter",
             "enableHttps": false,
             "nodeCacheCapable": false,
