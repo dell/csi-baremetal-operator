67b2ab15c701bb00f3ed1dc18e7cf156f9ae100a
Anton Yurkov <anton.yurkov@dell.com>
2021-10-12 07:36:32 -0700

    debug
    Signed-off-by: Anton Yurkov <anton.yurkov@dell.com>

--- pkg/controller.go
+++ pkg/controller.go
@@ -2,6 +2,7 @@ package pkg
 
 import (
 	"context"
+	"fmt"
 	"strconv"
 
 	"github.com/go-logr/logr"
@@ -43,6 +44,7 @@ type Controller struct {
 func (c *Controller) Update(ctx context.Context, csi *csibaremetalv1.Deployment, scheme *runtime.Scheme) error {
 	// create deployment
 	expected := createControllerDeployment(csi)
+	c.Logger.Info("Resources: " + expected.Spec.Template.Spec.Containers[0].Name + " " + fmt.Sprintf("%#v", expected.Spec.Template.Spec.Containers[0].Resources))
 	if err := controllerutil.SetControllerReference(csi, expected, scheme); err != nil {
 		return err
 	}
