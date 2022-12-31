1a4117ab740ff7f3f9ed10dc79751d874cb2e4e6
CraneShiEMC <64512450+CraneShiEMC@users.noreply.github.com>
2022-12-31 14:52:15 +0800

    Second scheduler extender (#147)
    * Add secondary scheduler feature flag for Openshift 4.10+, create second scheduler extender configmap and secondaryscheduler resourse cluster in csi deployment
    
    Signed-off-by: Shi, Crane <crane.shi@emc.com>
    
    * add feature flag secondaryScheduler in csi deployment crd
    
    Signed-off-by: Shi, Crane <crane.shi@emc.com>
    
    * add openshift secondaryscheduler type to scheme
    
    Signed-off-by: Shi, Crane <crane.shi@emc.com>
    
    * add RBAC role of openshift secondaryscheduler into csi op sa
    
    Signed-off-by: Shi, Crane <crane.shi@emc.com>
    
    * Add secondscheduler support in extender readiness watch
    
    Signed-off-by: Shi, Crane <crane.shi@emc.com>
    
    * retain origin openshift scheduler policy config
    
    Signed-off-by: Shi, Crane <crane.shi@emc.com>
    
    * only create secondaryscheduler cluster when it doesn't exist
    
    Signed-off-by: Shi, Crane <crane.shi@emc.com>
    
    * check secondaryscheduler existence in secondaryscheduler namespace
    
    Signed-off-by: Shi, Crane <crane.shi@emc.com>
    
    * dynamically extract masterNodeIp for secondary scheduler extender config
    
    Signed-off-by: Shi, Crane <crane.shi@emc.com>
    
    * Debug Master Node IP Used for Secondary Scheduler Extender Config
    
    Signed-off-by: Shi, Crane <crane.shi@emc.com>
    
    * keep secondscheduler extender ip not changed if the node online
    
    Signed-off-by: Shi, Crane <crane.shi@emc.com>
    
    * remove secondaryscheduler in csi uninstall
    
    Signed-off-by: Shi, Crane <crane.shi@emc.com>
    
    Signed-off-by: Shi, Crane <crane.shi@emc.com>

--- pkg/patcher/scheduler_patcher_openshift.go
+++ pkg/patcher/scheduler_patcher_openshift.go
@@ -4,10 +4,14 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	oov1 "github.com/openshift/api/operator/v1"
+	ssv1 "github.com/openshift/secondary-scheduler-operator/pkg/apis/secondaryscheduler/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"strings"
 
 	openshiftv1 "github.com/openshift/api/config/v1"
 	corev1 "k8s.io/api/core/v1"
+	k8sError "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -19,8 +23,91 @@ const (
 	openshiftNS     = "openshift-config"
 	openshiftConfig = "scheduler-policy"
 
-	openshiftPolicyFile = "policy.cfg"
+	openshiftPolicyFile   = "policy.cfg"
+	k8sMasterNodeLabelKey = "node-role.kubernetes.io/master"
 )
+
+func (p *SchedulerPatcher) getMasterNodeIP(ctx context.Context) (string, error) {
+	labelSelector := labels.SelectorFromSet(map[string]string{k8sMasterNodeLabelKey: ""})
+
+	var nodes corev1.NodeList
+	err := p.Client.List(ctx, &nodes, &client.ListOptions{LabelSelector: labelSelector})
+	if err != nil {
+		return "", err
+	}
+	var potentialMasterNodeIP string
+	if len(nodes.Items) > 0 {
+		for _, node := range nodes.Items {
+			nodeIP := ""
+			for _, addr := range node.Status.Addresses {
+				if addr.Type == corev1.NodeInternalIP {
+					nodeIP = addr.Address
+					break
+				}
+			}
+			if nodeIP != "" {
+				if p.OpenshiftMasterNodeIP == "" {
+					p.OpenshiftMasterNodeIP = nodeIP
+				}
+				if nodeIP == p.OpenshiftMasterNodeIP {
+					return p.OpenshiftMasterNodeIP, nil
+				}
+				if potentialMasterNodeIP == "" {
+					potentialMasterNodeIP = nodeIP
+				}
+			}
+		}
+		if potentialMasterNodeIP != "" {
+			return potentialMasterNodeIP, nil
+		}
+		return "", fmt.Errorf("no k8s master node ip found")
+	}
+	return "", fmt.Errorf("no k8s master node found")
+}
+
+func (p *SchedulerPatcher) patchOpenShiftSecondaryScheduler(ctx context.Context, csi *csibaremetalv1.Deployment) error {
+	masterNodeIP, err := p.getMasterNodeIP(ctx)
+	if err != nil {
+		return err
+	}
+	p.Log.Infof("Master Node IP Used: %s", masterNodeIP)
+
+	secondarySchedulerExtenderConfig := fmt.Sprintf(`apiVersion: kubescheduler.config.k8s.io/v1beta3
+kind: KubeSchedulerConfiguration
+leaderElection:
+  leaderElect: false
+profiles:
+  - schedulerName: csi-baremetal-scheduler
+extenders:
+  - urlPrefix: "http://%s:%s"
+    filterVerb: filter
+    prioritizeVerb: prioritize
+    weight: 1
+    enableHTTPS: false
+    nodeCacheCapable: false
+    ignorable: true`, masterNodeIP, csi.Spec.Scheduler.ExtenderPort)
+
+	expected := createSecondarySchedulerConfig(secondarySchedulerExtenderConfig)
+
+	// TODO csi can't control cm in another namespace https://github.com/dell/csi-baremetal/issues/470
+	// if err := controllerutil.SetControllerReference(csi, expected, scheme); err != nil {
+	// 	return err
+	// }
+
+	err = common.UpdateConfigMap(ctx, p.Clientset, expected, p.Log)
+	if err != nil {
+		return err
+	}
+
+	// try to patch
+	err = p.updateSecondaryScheduler(ctx, openshiftConfig)
+	if err != nil {
+		p.Log.Error(err, "Failed to patch Scheduler")
+		return err
+	}
+
+	return nil
+}
 
 func (p *SchedulerPatcher) patchOpenShift(ctx context.Context, csi *csibaremetalv1.Deployment) error {
 	openshiftPolicy := fmt.Sprintf(`{
@@ -56,6 +143,30 @@ func (p *SchedulerPatcher) patchOpenShift(ctx context.Context, csi *csibaremetal
 	if err != nil {
 		p.Log.Error(err, "Failed to patch Scheduler")
 		return err
+	}
+
+	return nil
+}
+
+func (p *SchedulerPatcher) unPatchOpenShiftSecondaryScheduler(ctx context.Context) error {
+	var errMsgs []string
+
+	// TODO Remove after https://github.com/dell/csi-baremetal/issues/470
+	cfClient := p.Clientset.CoreV1().ConfigMaps("openshift-secondary-scheduler-operator")
+	err := cfClient.Delete(ctx, "csi-baremetal-scheduler-config", metav1.DeleteOptions{})
+	if err != nil {
+		p.Log.Error(err, "Failed to delete Openshift Secondary Scheduler ConfigMap")
+		errMsgs = append(errMsgs, err.Error())
+	}
+
+	err = p.uninstallSecondaryScheduler(ctx)
+	if err != nil {
+		p.Log.Error(err, "Failed to unpatch Scheduler")
+		errMsgs = append(errMsgs, err.Error())
+	}
+
+	if len(errMsgs) != 0 {
+		return fmt.Errorf(strings.Join(errMsgs, "\n"))
 	}
 
 	return nil
@@ -100,6 +211,17 @@ func (p *SchedulerPatcher) retryPatchOpenshift(ctx context.Context, csi *csibare
 	return nil
 }
 
+func createSecondarySchedulerConfig(config string) *corev1.ConfigMap {
+	return &corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "csi-baremetal-scheduler-config",
+			Namespace: "openshift-secondary-scheduler-operator",
+		},
+		Data: map[string]string{"config.yaml": config},
+	}
+}
+
 func createOpenshiftConfig(policy string) *corev1.ConfigMap {
 	return &corev1.ConfigMap{
 		TypeMeta: metav1.TypeMeta{},
@@ -109,6 +231,54 @@ func createOpenshiftConfig(policy string) *corev1.ConfigMap {
 		},
 		Data: map[string]string{openshiftPolicyFile: policy},
 	}
+}
+
+func (p *SchedulerPatcher) updateSecondaryScheduler(ctx context.Context, config string) error {
+	secondaryScheduler := &ssv1.SecondaryScheduler{}
+
+	err := p.Client.Get(ctx, client.ObjectKey{Name: "cluster", Namespace: "openshift-secondary-scheduler-operator"}, secondaryScheduler)
+	if err != nil {
+		if k8sError.IsNotFound(err) {
+			secondaryScheduler = &ssv1.SecondaryScheduler{
+				TypeMeta: metav1.TypeMeta{},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "cluster",
+					Namespace: "openshift-secondary-scheduler-operator",
+				},
+				Spec: ssv1.SecondarySchedulerSpec{
+					OperatorSpec: oov1.OperatorSpec{
+						ManagementState:  "Managed",
+						OperatorLogLevel: "Normal",
+						LogLevel:         "Normal",
+					},
+					SchedulerConfig: "csi-baremetal-scheduler-config",
+					SchedulerImage:  "k8s.gcr.io/scheduler-plugins/kube-scheduler:v0.23.10",
+				},
+			}
+
+			err = p.Client.Create(ctx, secondaryScheduler)
+			if err != nil {
+				return err
+			}
+		} else {
+			return err
+		}
+	}
+
+	//name := sc.Spec.Policy.Name
+	//// patch when name is not set
+	//if name == "" {
+	//	sc.Spec.Policy.Name = config
+	//	// update scheduler cluster
+
+	//	return nil
+	//}
+	//// if name is set but not to CSI config name return error
+	//if name != config {
+	//	return errors.New("scheduler is already patched with the config name: " + name)
+	//}
+
+	return nil
 }
 
 func (p *SchedulerPatcher) patchScheduler(ctx context.Context, config string) error {
@@ -135,6 +305,21 @@ func (p *SchedulerPatcher) patchScheduler(ctx context.Context, config string) er
 		return errors.New("scheduler is already patched with the config name: " + name)
 	}
 
+	return nil
+}
+
+func (p *SchedulerPatcher) uninstallSecondaryScheduler(ctx context.Context) error {
+	secondaryScheduler := &ssv1.SecondaryScheduler{}
+
+	err := p.Client.Get(ctx, client.ObjectKey{Name: "cluster", Namespace: "openshift-secondary-scheduler-operator"}, secondaryScheduler)
+	if err != nil {
+		return err
+	}
+
+	p.Client.Delete(ctx, secondaryScheduler)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
