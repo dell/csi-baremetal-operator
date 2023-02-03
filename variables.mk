84533f543232201cdca8f99521b7216872f7db6d
yimingwangdell <yiming.wang@dell.com>
2023-02-03 17:30:25 +0800

    [ISSUE-940] upgrade kubectl image to fix CVE
    Signed-off-by: yimingwangdell <yiming.wang@dell.com>

--- variables.mk
+++ variables.mk
@@ -11,7 +11,7 @@ CRD_OPTIONS ?= "crd:trivialVersions=true"
 # image vars
 BASE_IMAGE ?= golang:1.17
 CRD_BUILD_IMAGE ?= ${REGISTRY}/csi-baremetal-pre-upgrade-crds:${TAG}
-KUBECTL_IMAGE ?=  bitnami/kubectl:1.23 # https://hub.docker.com/r/bitnami/kubectl
+KUBECTL_IMAGE ?=  bitnami/kubectl:1.25 # https://hub.docker.com/r/bitnami/kubectl
 
 ### version
 MAJOR            := 1
