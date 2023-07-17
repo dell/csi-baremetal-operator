87453eb21781a61c9d867363b32fa325b1f4244e
yimingwangdell <yiming.wang@dell.com>
2023-07-17 15:33:39 +0800

    [ISSUE-1038] 1.3.0 branched out, change version to 1.4.0.
    Signed-off-by: yimingwangdell <yiming.wang@dell.com>

--- variables.mk
+++ variables.mk
@@ -15,7 +15,7 @@ KUBECTL_IMAGE ?=  bitnami/kubectl:1.25 # https://hub.docker.com/r/bitnami/kubect
 
 ### version
 MAJOR            := 1
-MINOR            := 3
+MINOR            := 4
 PATCH            := 0
 PRODUCT_VERSION  ?= ${MAJOR}.${MINOR}.${PATCH}
 BUILD_REL_A      := $(shell git rev-list HEAD |wc -l)
