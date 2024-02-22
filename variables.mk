6eac267c6426d7bb2206bd746f281ff3cacc486b
Andrzej-Zukowski <150017073+Andrzej-Zukowski@users.noreply.github.com>
2024-02-22 10:46:31 +0100

    [Issue-208] 1.5.1 version (#209)
    Signed-off-by: Andrzej Zukowski <andrzej.zukowski@dell.com>

--- variables.mk
+++ variables.mk
@@ -16,7 +16,7 @@ KUBECTL_IMAGE ?=  bitnami/kubectl:1.29.1 # https://hub.docker.com/r/bitnami/kube
 ### version
 MAJOR            := 1
 MINOR            := 5
-PATCH            := 0
+PATCH            := 1
 PRODUCT_VERSION  ?= ${MAJOR}.${MINOR}.${PATCH}
 BUILD_REL_A      := $(shell git rev-list HEAD |wc -l)
 BUILD_REL_B      := $(shell git rev-parse --short HEAD)
